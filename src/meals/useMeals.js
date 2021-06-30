2548a2dee5070a6015dc1e26ec5034f38c26ea0a
darshan527 <darshan.hosamani6@gmail.com>
2021-07-01 00:53:37 +0530

    modified Date in return object

--- src/meals/useMeals.js
+++ src/meals/useMeals.js
@@ -1,20 +1,27 @@
 import { useState, useEffect } from "react";
 
 export const useMeals = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [rawMeals, setRawMeals] = useState([]);
 
-    useEffect(() => {
-        const loadMeals = async () => {
-            setIsLoading(true)
-            const respose = await fetch('/meals')
-            const meals = await respose.json()
-            setMeals(meals)
-            setIsLoading(false)
-        }
+  useEffect(() => {
+    const loadMeals = async () => {
+      setIsLoading(true);
+      const respose = await fetch("/meals");
+      const rawMealsResponse = await respose.json();
+      setRawMeals(rawMealsResponse);
+      setIsLoading(false);
+    };
 
-        loadMeals()
-    }, [])
+    loadMeals();
+  }, []);
 
-    return { isLoading, meals, setMeals }
-}
+  return {
+    isLoading,
+    meals: rawMeals.map((rawMeal) => ({
+      ...rawMeal,
+      plannedDate: new Date(rawMeal.plannedDate),
+    })),
+    setMeals: setRawMeals,
+  };
+};
