2e5bc81378ffacf09bda6dceff92fe992956f267
darshan527 <darshan.hosamani6@gmail.com>
2021-07-05 22:37:40 +0530

    added ingredients

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { useMeals, MealsList } from "../meals";
+import { useIngredients } from "../ingredients";
 
 export const HomePage = () => {
   const { meals, isLoading: isLoadingMeals, setMeals } = useMeals();
+  const {
+    ingredients,
+    isLoading: isLoadingIngredients,
+    setIngredients,
+  } = useIngredients();
 
   console.log(meals);
 
