3a6554814d43c1d463267631a98563e88e316410
darshan527 <darshan.hosamani6@gmail.com>
2021-07-13 21:28:53 +0530

    Added onDeleteMeal

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -5,18 +5,23 @@ import { useIngredients, IngredientsList } from "../ingredients";
 
 export const HomePage = () => {
   const { meals, isLoading: isLoadingMeals, setMeals } = useMeals();
+
   const {
     ingredients,
     isLoading: isLoadingIngredients,
     setIngredients,
   } = useIngredients();
 
-  console.log(meals);
+  const onDeleteMeal = async id => {
+    const response = await fetch(`/meals/${id}`, { method: 'delete' });
+    const updatedMeals = await response.json();
+    setMeals(updatedMeals);
+  }
 
   return (
     <div className="page-container">
       <div className="column">
-        <MealsList isLoading={isLoadingMeals} meals={meals} />
+        <MealsList isLoading={isLoadingMeals} meals={meals} onDelete={onDeleteMeal} />
       </div>
       <div className="column">
         <IngredientsList
