9571ff85d36f0d53a0ab6056820b54a624ca531a
darshan527 <darshan.hosamani6@gmail.com>
2021-07-20 11:49:04 +0530

    Added Shopping List Generation

--- src/shopping-list/ShoppingListPage.js
+++ src/shopping-list/ShoppingListPage.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { BackButton } from '../ui'
+import { useShoppingList } from './useShoppingList'
 
-export const ShoppingListPage = () => (
-    <h1>This is Shopping List Page</h1>
-);
+export const ShoppingListPage = () => {
+    const { isLoading, items } = useShoppingList()
+    return (
+        <div className="page">
+            <BackButton />
+            <div className="centered-container">
+                <h1>Shopping List:</h1>
+                {isLoading ?
+                    <p>Loading...</p>
+                    : items.map(itm => (
+                        <p key={itm}>{itm}</p>
+                    ))}
+            </div>
+        </div>
+    )
+}
